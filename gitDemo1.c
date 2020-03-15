7edc9eec3f0a77e0f2dc3dd53467fba5f3955f0a
ka1992 <goodtaesik@gmail.com>
2020-03-15 19:10:09 +0900

    create gitDemo1.c
    commands words for git

---
+++ gitDemo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	char title[100]="git key words[2]";
+	char keyWords[200]="commit -m 'message', commit -am 'message'";
+	char branch[200]="git branch exp, git checkout exp, git branch, ls -al, git branch, git log --branches --decorate, git log --branches --decorate --graph, git log master .. esp";
+	
+printf("\t%s\n\n %s\n\n %s\n",  title, keyWords, branch);
+
+
+    
+    return 0;
+
+}
