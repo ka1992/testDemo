6c82cfbdf36aaed4ab8fa1684a46deb83fe03691
ka1992 <goodtaesik@gmail.com>
2020-03-15 12:47:28 +0900

    message c lang file

---
+++ gitDemo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+ char title[100]="command key words[1]";
+ char str[200]="git init, git status, git add, git commit, git log";
+ printf("\t%s\n\n %s\n", title, str);
+    
+    return 0;
+
+}
